16b6458b5de646f6bcdbfd1882c22373b305f61a
Andras Toth <andras.toth@nexttech.ro>
2025-06-22 11:36:25 +0300

    Slightly better error handling.

--- src/main/java/com/example/bioweatherbackend/api/GlobalExceptionHandler.java
+++ src/main/java/com/example/bioweatherbackend/api/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
@@ -17,15 +18,21 @@ public class GlobalExceptionHandler{
     public ResponseEntity<ErrorResponse> handle(Exception ex){
 
         var statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        String message = null;
         if (ex instanceof NoHandlerFoundException) {
             statusCode = HttpStatus.NOT_FOUND.value();
         } else if (ex instanceof IllegalArgumentException) {
             statusCode = HttpStatus.BAD_REQUEST.value();
         } else if (ex instanceof IllegalStateException) {
             statusCode = HttpStatus.CONFLICT.value();
+        } else if (ex instanceof HttpClientErrorException) {
+            statusCode = ((HttpClientErrorException) ex).getStatusCode().value();
+            message = ((HttpClientErrorException) ex).getStatusText();
         }
 
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), statusCode);
+        String msg = message != null ? message : ex.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(msg, statusCode);
+
         return new ResponseEntity<>(errorResponse, HttpStatusCode.valueOf(statusCode));
     }
 }
